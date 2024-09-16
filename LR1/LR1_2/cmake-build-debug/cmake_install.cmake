749772f6e7d116de4129400837ba62bc0f48d910
masterkusok <fedyasetrakov@gmail.com>
2024-09-16 23:32:53 +0300

    moved tasks

--- LR1/LR1_2/cmake-build-debug/cmake_install.cmake
+++ LR1/LR1_2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/fedor/CODE/C/fundy/LR1_2
+# Install script for directory: /home/fedor/CODE/C/fundy/LR1/LR1_2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/fedor/CODE/C/fundy/LR1_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/fedor/CODE/C/fundy/LR1/LR1_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
